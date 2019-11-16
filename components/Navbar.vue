72dfdb59e8ce789d62a32bd57dc293ff49b54f70
junpeko5 <junpeko5@gmail.com>
2019-11-17 07:02:34 +0900

    Logout user

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -30,7 +30,7 @@
             <a to="/login" class="nav-link">{{ user.name }}</a>
           </li>
           <li class="nav-item">
-            <nuxt-link to="/logout" class="nav-link">Logout</nuxt-link>
+            <a @click.prevent="logout" class="nav-link">Logout</a>
           </li>
         </ul>
       </template>
@@ -40,6 +40,10 @@
 
 <script>
 export default {
-
+  methods: {
+    logout() {
+      this.$auth.logout()
+    }
+  }
 }
 </script>
