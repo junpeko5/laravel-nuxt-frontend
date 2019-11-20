89d3779f6e8c69856465b9b5c7f1bb4d4edabd85
junpeko5 <junpeko5@gmail.com>
2019-11-20 20:30:39 +0900

    Showing all topics

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light mb-5">
     <nuxt-link to="/" class="navbar-brand">Frontend</nuxt-link>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
--- pages/topics/index.vue
+++ pages/topics/index.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="container">
     <h2>Latest Topics</h2>
-    {{topics}}
+    <div v-for="(topic, index) in topics" :key="index" class="bg-light mt-5 mb-5" style="padding: 20px;">
+      <h2>{{topic.title}}</h2>
+      <p class="text-muted">{{topic.created_at}} by {{topic.user.name}}</p>
+      <div v-for="(content, index) in topic.post" :key="index" class="ml-5 content">
+        {{content.body}}
+        <p class="text-muted">{{content.created_at}} by {{content.user.name}}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,9 +21,17 @@ export default {
   },
   async asyncData(context) {
     let {data} = await context.$axios.$get('/topics')
+    console.log(data)
     return {
       topics: data
     }
   }
 }
 </script>
+
+<style scoped>
+  .content {
+      border-left: 10px solid white;
+      padding: 0 10px 0 10px;
+  }
+</style>
