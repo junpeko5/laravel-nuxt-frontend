bedad9482da3669efebff5cab0519dd7e0510879
junpeko5 <junpeko5@gmail.com>
2019-11-20 20:19:03 +0900

    Get all topic

---
+++ pages/topics/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <h2>Latest Topics</h2>
+    {{topics}}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      topics: []
+    }
+  },
+  async asyncData(context) {
+    let {data} = await context.$axios.$get('/topics')
+    return {
+      topics: data
+    }
+  }
+}
+</script>
