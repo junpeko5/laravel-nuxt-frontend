7e6aff491b5722306c49658a106b08adee656a06
junpeko5 <junpeko5@gmail.com>
2019-11-22 06:19:23 +0900

    Create Post

--- pages/topics/_id/index.vue
+++ pages/topics/_id/index.vue
@@ -17,6 +17,26 @@
         <p class="text-muted">{{content.created_at}} by {{content.user.name}}</p>
       </div>
     </div>
+    <div class="mt-5 ml-5" v-if="authenticated">
+      <form @submit.prevent="create">
+        <div class="form-group">
+          <label for="">
+            <b>Add a new Post</b>
+          </label>
+          <textarea
+          class="form-control"
+          placeholder="write something..."
+          v-model="body"></textarea>
+          <small v-if="errors.body"
+                class="form-text text-danger">
+            {{ errors.body[0] }}
+          </small>
+        </div>
+        <button class="btn btn-outline-primary">
+          Add a new post
+        </button>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -30,7 +50,16 @@ export default {
   async asyncData({$axios, params}) {
     const {data} = await $axios.$get(`/topics/${params.id}`)
     return {
-      topic: data
+      topic: data,
+      body: ''
+    }
+  },
+  methods: {
+    async create() {
+      await this.$axios.post(`/topics/${this.$route.params.id}/posts`, {
+        body: this.body
+      })
+      this.$router.push('/topics');
     }
   }
 }
